2891a55cd1d36a0b8c26823abfbd4c9ba9fddc6b
CRBogdan <bogdanrazvan00@yahoo.com>
2023-01-30 19:18:15 +0200

    register.jsp

---
+++ src/main/java/com/parking/parkinglot/servlets/Register.java
@@ -0,0 +1,21 @@
+package com.parking.parkinglot.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "Register", value = "/Register")
+public class Register extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/pages/register.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
+}
