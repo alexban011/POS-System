94e2f33f8d2e6c158de7119aa9ce9e0918b59da3
alexban011 <alexban011@gmail.com>
2023-02-01 19:43:33 +0200

    added permissions for CASHIER and ADMIN on SELL

--- src/main/java/com/parking/parkinglot/servlets/Sell.java
+++ src/main/java/com/parking/parkinglot/servlets/Sell.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-//@ServletSecurity(value = @HttpConstraint(rolesAllowed = {"WRITE_CARS"}))
+@ServletSecurity(value = @HttpConstraint(rolesAllowed = {"CASHIER", "ADMIN"}))
 @WebServlet(name = "Sell", value = "/Sell")
 public class Sell extends HttpServlet {
 
